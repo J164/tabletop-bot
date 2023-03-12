7ba4d6c1530a7015b2df5e39a95571ace8bd37fe
J164 <43323130+J164@users.noreply.github.com>
2023-03-12 17:56:15 -0500

    add card image merge function (untested)

---
+++ src/util/card-images.ts
@@ -0,0 +1,74 @@
+import { readdir, readFile } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
+import sharp from 'sharp';
+
+type CardCode = `${'2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' | 'J' | 'Q' | 'K' | 'A'}${'S' | 'C' | 'H' | 'D'}` | 'back' | 'XX' | 'X1' | 'X2' | 'XX';
+
+async function getCardImages() {
+	const images: Record<string, Buffer> = {};
+
+	const fileNames = await readdir('./static/img/cards', {
+		withFileTypes: false,
+	});
+
+	await Promise.all(
+		fileNames.map(async (fileName) => {
+			images[fileName] = await readFile(`${fileName}.png`);
+		}),
+	);
+	return images as Record<CardCode, Buffer>;
+}
+
+const cardImages = await getCardImages();
+
+export function getCardImage(code: CardCode) {
+	return Buffer.from(cardImages[code]);
+}
+
+const CARD_WIDTH = 226;
+const CARD_HEIGHT = 314;
+
+/**
+ * Merges a number of card images into one image
+ * @param cardCodes Array of card codes
+ * @param width Number of cards to fit in each row
+ * @returns A Promise that resolves to the merged image as a png buffer
+ */
+export async function mergeImages(cardCodes: CardCode[], width: number): Promise<Buffer> {
+	let numberInRow = 0;
+	let posX = 0;
+	let posY = 0;
+
+	const images = cardCodes.map((cardCode) => {
+		let x;
+
+		if (numberInRow === width) {
+			x = 0;
+			posX = CARD_WIDTH;
+			posY += CARD_HEIGHT;
+			numberInRow = 0;
+		} else {
+			x = posX;
+			posX += CARD_WIDTH;
+			numberInRow++;
+		}
+
+		return {
+			input: getCardImage(cardCode),
+			left: x,
+			top: posY,
+		};
+	});
+
+	return sharp({
+		create: {
+			width: CARD_WIDTH * width,
+			height: posY + CARD_HEIGHT,
+			background: { alpha: 0, r: 0, g: 0, b: 0 },
+			channels: 3,
+		},
+	})
+		.composite(images)
+		.toFormat('png')
+		.toBuffer();
+}
