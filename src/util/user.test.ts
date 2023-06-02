4227fa6a1c3e53d85a8e73b8df93a397f6b13b75
J164 <43323130+J164@users.noreply.github.com>
2023-06-02 14:18:35 -0500

    start user tests

--- src/util/user.test.ts
+++ src/util/user.test.ts
@@ -1,6 +1,19 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { fetchUser, User } from './user.js';
 
-describe('User', () => {
-	it.todo('should create a new User if one does not exist in the cache');
-	it.todo('should fetch the cached User if fetched previously');
+// TODO: environment variables for tests (tries to sign into MongoDB)
+describe.todo('User', () => {
+	it('should create a new User if one does not exist in the cache', () => {
+		expect(fetchUser('foo')).toBeInstanceOf(User);
+	});
+
+	it('should fetch the cached User if fetched previously', () => {
+		const user = fetchUser('foo');
+
+		// eslint-disable-next-line @typescript-eslint/dot-notation
+		user['bar'] = true;
+
+		// eslint-disable-next-line @typescript-eslint/dot-notation
+		expect(fetchUser('foo')['bar']).toBe(true);
+	});
 });
