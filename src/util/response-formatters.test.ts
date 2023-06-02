aa111ff9a9c2478eebfb71e5703dac077ed775f0
J164 <43323130+J164@users.noreply.github.com>
2023-06-02 14:03:01 -0500

    write response formatter tests

--- src/util/response-formatters.test.ts
+++ src/util/response-formatters.test.ts
@@ -1,9 +1,26 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { EmbedType, responseEmbed } from './response-formatters.js';
 
 describe('responseEmbed()', () => {
-	it.todo('should prepend the correct emoji');
-	it.todo('should set the correct color if not explicitly set');
-	it.todo('should not modify the color if explicity set in options');
+	it('should prepend the correct emoji', () => {
+		expect(responseEmbed(EmbedType.Info, 'foo').title).toBe('📄	foo');
+		expect(responseEmbed(EmbedType.Error, 'foo').title).toBe('❌	foo');
+		expect(responseEmbed(EmbedType.Success, 'foo').title).toBe('✅	foo');
+		expect(responseEmbed(EmbedType.Prompt, 'foo').title).toBe('❓	foo');
+		expect(responseEmbed(EmbedType.None, 'foo').title).toBe('foo');
+	});
+
+	it('should set the correct color if not explicitly set', () => {
+		expect(responseEmbed(EmbedType.Info, 'foo').color).toBe(0x00_99_ff);
+		expect(responseEmbed(EmbedType.Error, 'foo').color).toBe(0xff_00_00);
+		expect(responseEmbed(EmbedType.Success, 'foo').color).toBe(0x00_ff_00);
+		expect(responseEmbed(EmbedType.Prompt, 'foo').color).toBe(0xff_a5_00);
+		expect(responseEmbed(EmbedType.None, 'foo').color).toBeUndefined();
+	});
+
+	it('should not modify the color if explicity set in options', () => {
+		expect(responseEmbed(EmbedType.Info, 'foo', { color: 0xee_ff_ff }).color).toBe(0xee_ff_ff);
+	});
 });
 
 describe('responseOptions()', () => {
